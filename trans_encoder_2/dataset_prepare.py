06acd767df1de0817b02b92f16b0c58b31947ea4
William Han <willhan327@outlook.com>
2022-05-27 17:15:49 -0700

    data_set prepare

---
+++ trans_encoder_2/dataset_prepare.py
@@ -0,0 +1,55 @@
+class TextDataset(Dataset):
+  def __init__(self, texts, labels, tokenizer, max_len):
+    self.texts = texts
+    self.labels = labels
+    self.tokenizer = tokenizer
+    self.max_len = max_len
+
+  @property
+  def n_insts(self):
+    return len(self.labels)
+
+  @property
+  def text_len(self):
+    return 32
+
+  def __len__(self):
+    return self.n_insts
+
+  def __getitem__(self, item):
+    text = str(self.texts[item])
+    label = self.labels[item]
+
+    encoding = self.tokenizer.encode_plus(
+      text,
+      add_special_tokens=True,
+      max_length=self.max_len,
+      return_token_type_ids=False,
+      pad_to_max_length = True,
+      return_attention_mask=True
+      #return_tensors='pt',
+    )
+    return torch.FloatTensor(encoding['input_ids']).flatten(), torch.tensor(label, dtype=torch.long)
+
+
+class EEGDataset(Dataset):
+    def __init__(self, raw_data):
+
+        self._signal = torch.FloatTensor(raw_data[:, 1:])
+        self._label = torch.LongTensor(raw_data[:, 0])
+
+
+    @property
+    def n_insts(self):
+        ''' Property for dataset size '''
+        return len(self._label)
+
+    @property
+    def sig_len(self):
+        return self._signal.shape[1]
+
+    def __len__(self):
+        return self.n_insts
+
+    def __getitem__(self, idx):
+        return self._signal[idx], self._label[idx]
