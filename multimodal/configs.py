860494ca81f56e051f26ab1536bebcd452e936fa
William Han <willhan327@outlook.com>
2022-05-29 14:46:51 -0700

    main

--- multimodal/configs.py
+++
@@ -1,9 +0,0 @@
-MAX_LEN = 32
-epoch = 100
-class_num = 3
-batch_size = 128
-PRE_TRAINED_MODEL_NAME = 'bert-base-cased'
-emotion = 'happy_trans'
-csv = 'df.csv'
-eeg = ['delta', 'lowAlpha', 'highAlpha', 'lowBeta', 'highBeta',
-'lowGamma', 'middleGamma', 'theta']
